f58cb12c57535a94aa116510b5addaeaf7cb9b96
millag <millag43@gmail.com>
2013-10-17 17:06:42 +0100

    bug fixig: add m_maxDepth to Octree-terminate recursion on build step  + fix in findNearestPoint

--- src/Octree.cpp
+++ src/Octree.cpp
@@ -1,27 +1,32 @@
 #include <limits>
+#include <cassert>
+
 #include "Octree.h"
 
 
-Octree::Octree( const std::vector<Vec3>& pointCloud, unsigned resolution  ):m_resolution(resolution)
+Octree::Octree( const std::vector<Vec3>& pointCloud, unsigned resolution , unsigned maxDepth ):m_resolution(resolution),m_maxDepth(maxDepth)
 {
+    assert( resolution > 0 && maxDepth > 0 );
     OctreeNode* node = new OctreeNode();
-    m_depth = buildNode( pointCloud, *node );
+    m_depth = buildNode( pointCloud, 0u, *node );
     m_root = node;
 }
 
-//FIX:
-unsigned Octree::buildNode( const std::vector<Vec3>& pointCloud, OctreeNode& o_node)
+unsigned Octree::buildNode( const std::vector<Vec3>& pointCloud, unsigned depth , OctreeNode& o_node)
 {
+    ++depth;
     // if we hit the bottom
-    if ( pointCloud.size() <= m_resolution )
+    if ( pointCloud.size() <= m_resolution || depth >= m_maxDepth )
     {
         o_node.m_points.insert( o_node.m_points.begin(), pointCloud.begin(), pointCloud.end() );
-        return 1;
+        return depth;
     }
 
     o_node.m_midPoint = getBarycenterForPointCloud( pointCloud );
     o_node.m_children.resize( 8, NULL );
-    unsigned maxDepth = 0;
+
+    unsigned d;
+    unsigned maxDepth = depth;
     for ( unsigned i = 0; i< o_node.m_children.size(); ++i )
     {
         std::vector<Vec3> subCloud = filterPointsForOctant( pointCloud, o_node.m_midPoint, i );
@@ -30,11 +35,11 @@ unsigned Octree::buildNode( const std::vector<Vec3>& pointCloud, OctreeNode& o_n
 
         OctreeNode* node = new OctreeNode();
         o_node.m_children[i] = node;
-        unsigned depth = buildNode( subCloud, *node);
-        maxDepth = std::max( maxDepth,  depth);
+        d = buildNode( subCloud, depth, *node );
+        maxDepth = std::max( maxDepth, d );
     }
 
-    return maxDepth + 1;
+    return maxDepth;
 }
 
 
@@ -83,20 +88,18 @@ float Octree::findNearestPoint( const Vec3& point, const OctreeNode& node, Vec3&
 {
 //    leaf = hit the bottom
     if ( node.isLeaf() )
+    {
         return node.getNearestPoint( point, o_found );
-
-    float minSqrDist = std::numeric_limits<float>::max();
-
+    }
     unsigned octIndex = getOctantForPoint( point, node.m_midPoint );
-//    hit child leaf with no points in there
-    if ( node.m_children[octIndex] == NULL )
-        return minSqrDist;
+    float minSqrDist = std::numeric_limits<float>::max();
+//    find nearest point in search point's octatnt if it has any points in it
+    if ( node.m_children[octIndex] != NULL )
+    {
+        minSqrDist = findNearestPoint( point, *node.m_children[octIndex], o_found );
+    }
 
-
-//    find nearest point in search point's octatnt
-    minSqrDist = findNearestPoint( point, *node.m_children[octIndex], o_found );
-
-//    check if we intersect with any of the plane axis
+//    check if we intersect with any of the axis planes
     Vec3 p = (point - node.m_midPoint);
     p.m_x *= p.m_x;
     p.m_y *= p.m_y;
@@ -106,9 +109,10 @@ float Octree::findNearestPoint( const Vec3& point, const OctreeNode& node, Vec3&
 
 //    no intersection found
     if ( !mask )
+    {
         return minSqrDist;
-
-//    we have an intersection => loop through all children and check every octant if it is interesting
+    }
+//    we have an intersection => loop through all children and check every octant for intersection
     float dist = minSqrDist;
     Vec3 nearest = o_found;
     for ( unsigned i = 0; i < node.m_children.size(); ++i)
@@ -117,7 +121,7 @@ float Octree::findNearestPoint( const Vec3& point, const OctreeNode& node, Vec3&
         if ( node.m_children[i] == NULL )
             continue;
 
-//        tricky and unreadable :P
+//      check for intersection with current octatnt =>  tricky and unreadable :P
         if ( ((i & mask)^(octIndex & mask)) != 0  && (((i & (~mask))^(octIndex & (~mask))) == 0) )
         {
             dist = findNearestPoint( point, *node.m_children[i], nearest );
@@ -133,20 +137,7 @@ float Octree::findNearestPoint( const Vec3& point, const OctreeNode& node, Vec3&
 }
 
 
-//    float dist = minSqrDist;
-//    Vec3 nearest = o_found;
-//    for ( unsigned i = 0; i < node.m_children.size(); ++i ) {
-//        if ( node.m_children[i] == NULL )
-//            continue;
 
-//        dist = findNearestPoint( point, *(node.m_children[i]), nearest );
-//        if ( dist < minSqrDist ) {
-//            minSqrDist = dist;
-//            o_found = nearest;
-//        }
-//    }
-
-//    return minSqrDist;
 
 float OctreeNode::getNearestPoint( const Vec3& point, Vec3& o_found ) const
 {
